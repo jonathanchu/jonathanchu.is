2c52d71fa32b44aad188290842180efd059b3b76
Jonathan Chu <jonathan.chu@me.com>
2015-03-24 02:42:48 -0400

    Remove feeds, add in minor info.

--- pelicanconf.py
+++ pelicanconf.py
@@ -6,22 +6,16 @@ from os import environ
 
 AUTHOR = 'Jonathan Chu'
 SITENAME = 'jonathanchu.is'
+EMAIL = ''
 SITEURL = environ.get('PELICAN_SITEURL', 'http://jonathanchu.is')
 
 PATH = 'content'
 
 TIMEZONE = 'America/New_York'
-
 DEFAULT_LANG = 'en'
-
-# Feed generation is usually not desired when developing
-FEED_ALL_ATOM = None
-CATEGORY_FEED_ATOM = None
-TRANSLATION_FEED_ATOM = None
-AUTHOR_FEED_ATOM = None
-AUTHOR_FEED_RSS = None
 
 DEFAULT_PAGINATION = 10
 
-# Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
+RELATIVE_URLS = False
+
+MIXPANEL = ''
