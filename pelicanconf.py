5c2fb355e126dcac9f4b77558514dcd7b22eb6ae
Jonathan Chu <jonathan.chu@me.com>
2015-03-25 01:41:54 -0400

    Customize URL settings for posts, tags, pages.

--- pelicanconf.py
+++ pelicanconf.py
@@ -18,4 +18,21 @@ DEFAULT_PAGINATION = 10
 
 RELATIVE_URLS = False
 
+POSTS_URL = 'posts/'
+POSTS_INDEX_SAVE_AS = 'posts/index.html'
+
+ARTICLE_PATHS = ('posts',)
+ARTICLE_URL = 'posts/{slug}/'
+ARTICLE_SAVE_AS = 'posts/{slug}/index.html'
+
+TAG_URL = 'posts/tags/{slug}/'
+TAG_SAVE_AS = 'posts/tags/{slug}/index.html'
+TAGS_SAVE_AS = 'posts/tags/index.html'
+
+PAGE_URL = '{slug}/'
+PAGE_SAVE_AS = '{slug}/index.html'
+
+FEED_RSS = 'posts/feed/latest'
+FEED_ATOM = 'posts/feed/latest.atom'
+
 MIXPANEL = ''
